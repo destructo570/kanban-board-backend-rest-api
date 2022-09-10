e6d0502166c5d0c980bd962d8a3add8dfb9a278d
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-09-10 12:49:47 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const authController = require("../controllers/auth");
 const router = express.Router();
-const { body } = require("express-validator");
-const User = require("../models/user");
 const { signupValidator } = require("../middleware/validators");
 
 router.post("/signup", signupValidator, authController.signup);
